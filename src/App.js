db10125b774cc046466c2069abff8f9514d443e2
kscarby <sabrina.keito@gmail.com>
2025-04-25 15:18:40 -0300

    feat components and routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/Header.js'
+import Body from './components/Body.js'
+import Footer from './components/Footer.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/header' element={<Header />}></Route>
+          <Route path='/body' element={<Body />}></Route>
+          <Route path='/footer' element={<Footer />}></Route>
+        </Routes>
+      </BrowserRouter>      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Toolbar = () => {
+  return (
+    <div className='toolbar-container'>
+      <h1 className='title'>{}</h1>
+      <h2 className='subtitle'>{}</h2>
+      <p className='bio'></p>      
+    </div>
+  )
+}
+
+export default Toolbar
