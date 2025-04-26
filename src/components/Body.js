a15f6fb7ca7aacf73eddc372e2b08011d230c2a5
kscarby <sabrina.keito@gmail.com>
2025-04-25 21:02:51 -0300

    feat styles from Header

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div>
+        <h1>Sobre (About Me)</h1>
+        <h1>Portfólio (Work/Portfolio)</h1>
+        <h1>Serviços (Services)</h1>
+        <h1>Feedbacks</h1>
+        <h1>Contato (Contact)</h1>
+    </div>
   )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import { Portifolio } from './Informations';
 
+import avatar from '../assets/avatar.jpg'
+import linkedin from '../assets/linkedin.png'
+import instagram from '../assets/instagram.png'
+import github from '../assets/github.png'
+import '../styles/Header.css'
 const Header = () => {
   return (
     <div className='header-container'>
+      <div className='container-profile'>
+        <img className='img-profile' alt='img-profile' src={avatar}></img>
+      </div>
       {Portifolio.map((text, index) => (
-        <div key={index}>
-          <h1 className='title'>Olá, meu nome é {text.title}</h1>
-          <h2 className='subtitle'>{text.subtitle}</h2>
+        <div className='container-item' key={index}>
+          <h1 className='title'>Olá, sou a {text.title}</h1>
+          <h2 className='subtitle'>Sou formada em {text.subtitle}</h2>
           <p className='bio'>{text.bio}</p>
         </div>
-      ))}     
+      ))}
+      <div className='container-social'>
+        <img alt='linkedin' className='social' src={linkedin}></img>
+        <img alt='github' className='social' src={github}></img>
+        <img alt='instagram' className='social'src={instagram}></img>
+      </div>
     </div>
   );
 };
